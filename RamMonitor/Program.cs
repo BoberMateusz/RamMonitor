9438ef57f2c97e444f3d22f18a6bffd5a945f502
Mateusz Bober <mateusz.bober@gmail.com>
2024-10-05 12:46:50 +0200

    Added comments

--- RamMonitor/Program.cs
+++ RamMonitor/Program.cs
@@ -7,16 +7,17 @@ using System.Threading;
 
 namespace RamMonitor
 {
+    /// Monitors RAM usage and writes data to a database at specified intervals.
     class Program
     {
         static void Main(string[] args)
         {
-            int operationLimit = Convert.ToInt32(ConfigurationManager.AppSettings["operationLimit"]);
-            int monitorDelay = Convert.ToInt32(ConfigurationManager.AppSettings["monitorDelay"]);
+            int operationLimit = Convert.ToInt32(ConfigurationManager.AppSettings["operationLimit"]); // Maximum number of monitoring iterations
+            int monitorDelay = Convert.ToInt32(ConfigurationManager.AppSettings["monitorDelay"]); // Delay between readings (in milliseconds)
             Console.WriteLine($"Monitoring RAM every {monitorDelay}ms, will stop after {operationLimit} iterations");
             for (int i = 0; i < operationLimit; i++)
             {
-                var memoryUsage = getTotalMemoryUsage();
+                var memoryUsage = GetTotalMemoryUsage();
                 var currentTime = DateTime.Now;
 
                 WriteToDatabase(memoryUsage, currentTime);
@@ -25,7 +26,7 @@ namespace RamMonitor
             }
         }
 
-        private static double getTotalMemoryUsage()
+        private static double GetTotalMemoryUsage()
         {
             var processes = Process.GetProcesses();
             return processes.Sum(p => p.WorkingSet64);
